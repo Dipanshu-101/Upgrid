a4e2d9d6ebeae408ece878c0d6c48f3fde6bc14e
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-08-30 14:08:36 +0000

    CHORE: fix exports

--- apps/pusher/index.ts
+++ apps/pusher/index.ts
@@ -1,14 +1,21 @@
 import {prismaClient} from "store/client";
+import {xAddBulk} from "redisstream/client";
+
+async function main() {
+       let websites = await prismaClient.website.findMany({
+          select : {
+          url: true,
+          id: true
+          }
+        })   
+        
+        await xAddBulk(websites.map( w => ({
+            url: w.url,
+            id: w.id
+        })));
 
 
-function main() {
- let websites =prismaClient.website.findMany({
-select : {
-    url: true,
-    id: true
-}
-})   
-   redis.xPush
+
 }
 setInterval(()=>{
     main()
