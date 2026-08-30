bf00daa6059b97ac7e3897d81ce3fc4eb4f4953b
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-08-30 13:16:12 +0000

    DB: Redis Client

--- apps/pusher/index.ts
+++ apps/pusher/index.ts
@@ -2,10 +2,14 @@ import {prismaClient} from "store/client";
 
 
 function main() {
- 
-}   
-
-
+ let websites =prismaClient.website.findMany({
+select : {
+    url: true,
+    id: true
+}
+})   
+   redis.xPush
+}
 setInterval(()=>{
     main()
 }, 3*1000);
