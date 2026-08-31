4d49308bedbaae47579e6bd6c13e782417fbe3b1
Dipanshu <deshmukhdipanshu101@gmail.com>
2026-08-31 12:00:10 +0000

    BE: Worker Refactor

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,11 +1,18 @@
+import {xAckBulk ,xReadGroup } from 'redisstream/client';
+
 const REGION_ID = process.env.REGION_ID;
 const WORKER_ID = process.env.WORKER_ID;
 
+if (!REGION_ID || !WORKER_ID) {
+  throw new Error('REGION_ID and WORKER_ID environment variables must be set');
+}
 async function main() {
- while(1) {
-    
-  }
+//  while(1) {
+    //@ts-ignore
+    const response = xReadGroup(REGION_ID, WORKER_ID);
+    //@ts-ignore
+    xAckBulk(REGION_ID,"a");
 }
 
-
+// }
 main()
